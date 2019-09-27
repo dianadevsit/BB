dafef15c0961fcc97ec68f98a3461f0e65bb7102
Dimitrios Tsioutsiopoulos <dimitrios.g0503@gmail.com>
2019-09-26 21:03:24 -0400

    commented the different routes brainsormed by patrick and dimi in class- just need to be implemented once the database is pushed and merged

--- Unsolved/routes/apiRoutes.js
+++ Unsolved/routes/apiRoutes.js
@@ -1,6 +1,30 @@
 var db = require("../models");
 
 module.exports = function(app) {
+
+//Logging in
+  //-->Get Route
+    //--> Display login page
+
+//Display Homepage
+  //-->Get Route
+    //-->Display Homepage By Default
+
+//Edit User Page
+  //-->Post Route
+    //--> edit aspects of the page aside from skills
+
+//Edit Skills
+  //-->Get all current skills/Ratings, and form to edit rating
+    //-->Submit Changes
+      //-->Post Route
+
+//Add Skills 
+  //-->Get Current Skill list & List of Available other skills
+    //-->Submit new skill and initial rating
+      //-->Post Route
+
+  
   // Get all examples
   app.get("/api/examples", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
