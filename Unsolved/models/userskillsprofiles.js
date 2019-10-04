a90833770144796e27bf93a616a19f87fc36ba10
dwlam8790 <49571438+dwlam8790@users.noreply.github.com>
2019-10-03 21:26:03 -0400

    moved things out of unsolved folder removed package-lock.json and added it to gitignore

--- Unsolved/models/userskillsprofiles.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-  var UserSkillsProfiles = sequelize.define("userSkillsProfiles", {
-    userID: DataTypes.STRING,
-    skillID: DataTypes.INTEGER,
-    ratingID: DataTypes.INTEGER
-  });
-
-  return UserSkillsProfiles;
-};
